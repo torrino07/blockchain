315376144c31cebbde332adc048980384627f3df
DGtalledo <61323407+torrino07@users.noreply.github.com>
2023-12-21 11:25:14 +0100

    test

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
-const ALCHEMY_API_URL="https://eth-goerli.g.alchemy.com/v2/9kQk--dWFauG2QLeXqBrc9jwrIYt2r-j"
+const ALCHEMY_API_URL="https://eth-sepolia.g.alchemy.com/v2/Gk8XenOHkElitZ4N6E60lc3qKsZcXWB4"
 const PRIVATE_KEY="96384276a09231d3b185cb9c2ced0d4de4a3231f4f5c8486c9ef5177ecd16a57"
 
 module.exports = {
