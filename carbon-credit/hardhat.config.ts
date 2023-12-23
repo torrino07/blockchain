1a3aab99428564612a78ece13f6d972a22b3f331
DGtalledo <61323407+torrino07@users.noreply.github.com>
2023-12-23 22:44:07 +0100

    added hard.config.ts

--- carbon-credit/hardhat.config.ts
+++ carbon-credit/hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const ALCHEMY_API_URL = "https://eth-sepolia.g.alchemy.com/v2/Gk8XenOHkElitZ4N6E60lc3qKsZcXWB4";
+const PRIVATE_KEY = "96384276a09231d3b185cb9c2ced0d4de4a3231f4f5c8486c9ef5177ecd16a57";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
